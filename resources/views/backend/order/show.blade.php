ffa2c669e7f512cd0e2ae06f81e29627af3beb98
Junior Pezantes <werras04@gmail.com>
2023-11-22 22:35:19 -0500

    fix: dompdf

--- resources/views/backend/order/show.blade.php
+++ resources/views/backend/order/show.blade.php
@@ -1,156 +1,172 @@
 @extends('backend.layouts.master')
 
-@section('title','Order Detail')
+@section('title', 'Order Detail')
 
 @section('main-content')
-<div class="card">
-<h5 class="card-header">Pedido <a href="{{route('order.pdf',$order->id)}}" class=" btn btn-sm btn-primary shadow-sm float-right"><i class="fas fa-download fa-sm text-white-50"></i> Generar PDF</a>
-  </h5>
-  <div class="card-body">
-    @if($order)
-    <table class="table table-striped table-hover">
-      <thead>
-        <tr>
-            <th>Numero</th>
-            <th>Pedido</th>
-            <th>Nombre</th>
-            <th>Correo</th>
-            <th>Cantidad</th>
-            <th>Envio</th>
-            <th>Monto total</th>
-            <th>Estado</th>
-            <th>Accion</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-            <td>{{$order->id}}</td>
-            <td>{{$order->order_number}}</td>
-            <td>{{$order->first_name}} {{$order->last_name}}</td>
-            <td>{{$order->email}}</td>
-            <td>{{$order->quantity}}</td>
-            <td>S/{{$order->shipping->price}}</td>
-            <td>S/{{number_format($order->total_amount,2)}}</td>
-            <td>
-                @if($order->status=='new')
-                  <span class="badge badge-primary">{{$order->status}}</span>
-                @elseif($order->status=='process')
-                  <span class="badge badge-warning">{{$order->status}}</span>
-                @elseif($order->status=='delivered')
-                  <span class="badge badge-success">{{$order->status}}</span>
-                @else
-                  <span class="badge badge-danger">{{$order->status}}</span>
-                @endif
-            </td>
-            <td>
-                <a href="{{route('order.edit',$order->id)}}" class="btn btn-primary btn-sm float-left mr-1" style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit" data-placement="bottom"><i class="fas fa-edit"></i></a>
-                <form method="POST" action="{{route('order.destroy',[$order->id])}}">
-                  @csrf
-                  @method('delete')
-                      <button class="btn btn-danger btn-sm dltBtn" data-id={{$order->id}} style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
-                </form>
-            </td>
+    <div class="card">
+        <h5 class="card-header">Pedido 
+          <a href="{{ route('order.pdf', $order->id) }}" class=" btn btn-sm btn-primary shadow-sm float-right">
+            <i class="fas fa-download fa-sm text-white-50"></i> Generar PDF
+          </a>
+        </h5>
+        <div class="card-body">
+            @if ($order)
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>Numero</th>
+                            <th>Pedido</th>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                            <th>Cantidad</th>
+                            <th>Envio</th>
+                            <th>Monto total</th>
+                            <th>Estado</th>
+                            <th>Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ $order->id }}</td>
+                            <td>{{ $order->order_number }}</td>
+                            <td>{{ $order->first_name }} {{ $order->last_name }}</td>
+                            <td>{{ $order->email }}</td>
+                            <td>{{ $order->quantity }}</td>
+                            <td>S/{{ $order->shipping->price }}</td>
+                            <td>S/{{ number_format($order->total_amount, 2) }}</td>
+                            <td>
+                                @if ($order->status == 'new')
+                                    <span class="badge badge-primary">{{ $order->status }}</span>
+                                @elseif($order->status == 'process')
+                                    <span class="badge badge-warning">{{ $order->status }}</span>
+                                @elseif($order->status == 'delivered')
+                                    <span class="badge badge-success">{{ $order->status }}</span>
+                                @else
+                                    <span class="badge badge-danger">{{ $order->status }}</span>
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('order.edit', $order->id) }}"
+                                    class="btn btn-primary btn-sm float-left mr-1"
+                                    style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip" title="edit"
+                                    data-placement="bottom"><i class="fas fa-edit"></i></a>
+                                <form method="POST" action="{{ route('order.destroy', [$order->id]) }}">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-danger btn-sm dltBtn" data-id={{ $order->id }}
+                                        style="height:30px; width:30px;border-radius:50%" data-toggle="tooltip"
+                                        data-placement="bottom" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                                </form>
+                            </td>
 
-        </tr>
-      </tbody>
-    </table>
+                        </tr>
+                    </tbody>
+                </table>
 
-    <section class="confirmation_part section_padding">
-      <div class="order_boxes">
-        <div class="row">
-          <div class="col-lg-6 col-lx-4">
-            <div class="order-info">
-              <h4 class="text-center pb-4">INFORMACION DE PEDIDO</h4>
-              <table class="table">
-                    <tr class="">
-                        <td>Numero d epedido</td>
-                        <td> : {{$order->order_number}}</td>
-                    </tr>
-                    <tr>
-                        <td>Dato del pedido</td>
-                        <td> : {{$order->created_at->format('D d M, Y')}} at {{$order->created_at->format('g : i a')}} </td>
-                    </tr>
-                    <tr>
-                        <td>Cantidad</td>
-                        <td> : {{$order->quantity}}</td>
-                    </tr>
-                    <tr>
-                        <td>Estado de el pedido</td>
-                        <td> : {{$order->status}}</td>
-                    </tr>
-                    <tr>
-                        <td>Costo de envio</td>
-                        <td> : S/ {{$order->shipping->price}}</td>
-                    </tr>
-                    <tr>
-                      <td>Cupon</td>
-                      <td> : S/ {{number_format($order->coupon,2)}}</td>
-                    </tr>
-                    <tr>
-                        <td>Monto total</td>
-                        <td> : S/ {{number_format($order->total_amount,2)}}</td>
-                    </tr>
-                    <tr>
-                        <td>Metodo de pago</td>
-                        <td> : @if($order->payment_method=='cod') Contra entrega @else Paypal @endif</td>
-                    </tr>
-                    <tr>
-                        <td>Estado de pago</td>
-                        <td> : {{$order->payment_status}}</td>
-                    </tr>
-              </table>
-            </div>
-          </div>
+                <section class="confirmation_part section_padding">
+                    <div class="order_boxes">
+                        <div class="row">
+                            <div class="col-lg-6 col-lx-4">
+                                <div class="order-info">
+                                    <h4 class="text-center pb-4">INFORMACION DE PEDIDO</h4>
+                                    <table class="table">
+                                        <tr class="">
+                                            <td>Numero d epedido</td>
+                                            <td> : {{ $order->order_number }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Dato del pedido</td>
+                                            <td> : {{ $order->created_at->format('D d M, Y') }} at
+                                                {{ $order->created_at->format('g : i a') }} </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Cantidad</td>
+                                            <td> : {{ $order->quantity }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Estado de el pedido</td>
+                                            <td> : {{ $order->status }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Costo de envio</td>
+                                            <td> : S/ {{ $order->shipping->price }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Cupon</td>
+                                            <td> : S/ {{ number_format($order->coupon, 2) }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Monto total</td>
+                                            <td> : S/ {{ number_format($order->total_amount, 2) }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Metodo de pago</td>
+                                            <td> : @if ($order->payment_method == 'cod')
+                                                    Contra entrega
+                                                @else
+                                                    Paypal
+                                                @endif
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Estado de pago</td>
+                                            <td> : {{ $order->payment_status }}</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
 
-          <div class="col-lg-6 col-lx-4">
-            <div class="shipping-info">
-              <h4 class="text-center pb-4">INFORMACION DE ENVIOo</h4>
-              <table class="table">
-                    <tr class="">
-                        <td>Nombre completo</td>
-                        <td> : {{$order->first_name}} {{$order->last_name}}</td>
-                    </tr>
-                    <tr>
-                        <td>Correo</td>
-                        <td> : {{$order->email}}</td>
-                    </tr>
-                    <tr>
-                        <td>Celular</td>
-                        <td> : {{$order->phone}}</td>
-                    </tr>
-                    <tr>
-                        <td>Direccion</td>
-                        <td> : {{$order->address1}}, {{$order->address2}}</td>
-                    </tr>
-                    <tr>
-                        <td>Distrito</td>
-                        <td> : {{$order->country}}</td>
-                    </tr>
-                    <tr>
-                        <td>Codigo postal</td>
-                        <td> : {{$order->post_code}}</td>
-                    </tr>
-              </table>
-            </div>
-          </div>
+                            <div class="col-lg-6 col-lx-4">
+                                <div class="shipping-info">
+                                    <h4 class="text-center pb-4">INFORMACION DE ENVIOo</h4>
+                                    <table class="table">
+                                        <tr class="">
+                                            <td>Nombre completo</td>
+                                            <td> : {{ $order->first_name }} {{ $order->last_name }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Correo</td>
+                                            <td> : {{ $order->email }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Celular</td>
+                                            <td> : {{ $order->phone }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Direccion</td>
+                                            <td> : {{ $order->address1 }}, {{ $order->address2 }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Distrito</td>
+                                            <td> : {{ $order->country }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Codigo postal</td>
+                                            <td> : {{ $order->post_code }}</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            @endif
+
         </div>
-      </div>
-    </section>
-    @endif
-
-  </div>
-</div>
+    </div>
 @endsection
 
 @push('styles')
-<style>
-    .order-info,.shipping-info{
-        background:#ECECEC;
-        padding:20px;
-    }
-    .order-info h4,.shipping-info h4{
-        text-decoration: underline;
-    }
+    <style>
+        .order-info,
+        .shipping-info {
+            background: #ECECEC;
+            padding: 20px;
+        }
 
-</style>
+        .order-info h4,
+        .shipping-info h4 {
+            text-decoration: underline;
+        }
+    </style>
 @endpush
